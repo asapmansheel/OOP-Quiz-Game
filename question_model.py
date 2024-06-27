27159873cfd1bca9beeabd315f88d4a6bc3ff752
asapmansheel <70127478+asapmansheel@users.noreply.github.com>
2024-06-27 13:46:46 -0400

    Update question_model.py

--- question_model.py
+++ question_model.py
@@ -1,1 +1,6 @@
-class Question:        def __init__(self,text,answer):        self.text = text        self.answer = answer        
+class Question:
+    
+    def __init__(self,text,answer):
+        self.text = text
+        self.answer = answer
+        
