a3bbcb86d2246691f9269ccb5d00b1a2a639d829
asapmansheel <70127478+asapmansheel@users.noreply.github.com>
2024-06-27 13:45:38 -0400

    Add files via upload

---
+++ data.py
@@ -0,0 +1,83 @@
+question_data = [
+        {
+            "type":"boolean",
+            "difficulty":"easy",
+            "category":"Geography",
+            "question":"Greenland is almost as big as Africa.",
+            "correct_answer":"False",
+            "incorrect_answers":["True"]
+        },
+        {
+            "type":"boolean",
+            "difficulty":"easy",
+            "category":"Geography",
+            "question":"A group of islands is called an &#039;archipelago&#039;.",
+            "correct_answer":"True",
+            "incorrect_answers":["False"]
+        },
+        {
+            "type":"boolean",
+            "difficulty":"easy",
+            "category":"Geography",
+            "question":"Nova Scotia is on the east coast of Canada.",
+            "correct_answer":"True",
+            "incorrect_answers":["False"]
+        },
+        {
+            "type":"boolean",
+            "difficulty":"easy",
+            "category":"Geography",
+            "question":"St. Louis is the capital of the US State Missouri.",
+            "correct_answer":"False",
+            "incorrect_answers":["True"]
+        },
+        {
+            "type":"boolean",
+            "difficulty":"easy",
+            "category":"Geography",
+            "question":"Hungary is the only country in the world beginning with H.",
+            "correct_answer":"False",
+            "incorrect_answers":["True"]
+        },
+        {
+            "type":"boolean",
+            "difficulty":"easy",
+            "category":"Geography",
+            "question":"There are no deserts in Europe.",
+            "correct_answer":"True",
+            "incorrect_answers":["False"]
+        },
+        {
+            "type":"boolean",
+            "difficulty":"easy",
+            "category":"Geography",
+            "question":"Greenland is covered with grass and Iceland covered with ice.",
+            "correct_answer":"False",
+            "incorrect_answers":["True"]
+        },
+        {
+            "type":"boolean",
+            "difficulty":"easy",
+            "category":"Geography",
+            "question":"Rhode Island is actually located on the US mainland, despite its name.",
+            "correct_answer":"True",
+            "incorrect_answers":["False"]
+        },
+        {
+            "type":"boolean",
+            "difficulty":"easy",
+            "category":"Geography",
+            "question":"San Marino is the only country completely surrounded by another country.",
+            "correct_answer":"False",
+            "incorrect_answers":["True"]
+        },
+        {
+            "type":"boolean",
+            "difficulty":"easy",
+            "category":"Geography",
+            "question":"Vatican City is a country.",
+            "correct_answer":"True",
+            "incorrect_answers":["False"]
+        }
+    ]
+    
